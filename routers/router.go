182e3c7773d3d9437dccb7b34bfbbb81d80b2d4f
Pramod <dev2@itneducation.com>
2025-01-13 23:23:41 +0530

    Installed and creating backend system using beego framework

---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"booking-data-integrationn-system/controllers"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
