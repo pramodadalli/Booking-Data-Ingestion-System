a3a95bae84217c08a2a60f37fa49f51fae43ba76
pramodadalli <pramodia003@gmil.com>
2025-01-14 00:27:25 +0530

    Add BookingController with CRUD operations - Implement CreateBooking to handle booking creation. - Implement GetBookings to retrieve all bookings with optional filters. - Implement GetBookingByID to fetch booking details by ID. - Implement DeleteBooking to remove a booking by ID.

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,13 @@
 package routers
 
 import (
-	"booking-data-integrationn-system/controllers"
-	beego "github.com/beego/beego/v2/server/web"
+    "booking-data-integration-system/controllers"
+     "github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+    beego.Router("/bookings", &controllers.BookingController{}, "post:CreateBooking")
+    beego.Router("/bookings", &controllers.BookingController{}, "get:GetBookings")
+    beego.Router("/bookings/:id", &controllers.BookingController{}, "get:GetBookingByID")
+    beego.Router("/bookings/:id", &controllers.BookingController{}, "delete:DeleteBooking")
 }
